22fd70d468e08a44f1a83a505aebc09f930fcb79
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-07-14 11:05:24 -0400

    Solution to exercise 2

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const { handleClients, handleClient, addNewClient,
+        removeClient, handle404 } = require('./handlers/clientHandlers.js');
+
 express()
   .use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -18,6 +21,10 @@ express()
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
 
-  // endpoints
+  .get('/clients', handleClients)
+  .get('/client/:id', handleClient)
+  .post('/new-client', addNewClient)
+  .delete('/client/:id', removeClient)
+  .get('/*', handle404)
 
   .listen(8000, () => console.log(`Listening on port 8000`));
